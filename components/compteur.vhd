2da1c3d39d1e14071c74b191cffbdcf8fab8a873
LePetitPrince <jacquetped@gmail.com>
2021-09-20 10:59:04 +0200

    added all

--- components/compteur.vhd
+++ components/compteur.vhd
@@ -1,29 +1,38 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
-use ieee.std_logic_arith.all;
-
-
-
-entity compteur is 
-	port (
-		Clk, En, ARst : in std_logic;
-		Q	: inout bit_vector (7 downto 0)
-	);
-end entity compteur;
-
-architecture rtl of compteur is
--- on decl	are les variables d e l'architecture avant le premier begin
+ 
+ 
+entity Compt_1 is 
+    generic ( N : integer);
+    port (
+        clk, en, arst_n, SRst: in std_logic;
+        q   : out std_logic_vector (N-1 downto 0)
+        );
+end entity Compt_1;
+ 
+architecture rtl of Compt_1 is
+-- on decl  are les variables d e l'architecture avant le premier begin
+ 
+signal Q1   : std_logic_vector (n-1 downto 0);
+ 
+ 
 begin
-	pcompteur : process(Clk, ARst, En)
-	begin 
-		 if ARst = '0' then 
-			Q <= "00000000";
-		 elsif  rising_edge(Clk) then
-			 if En = '1' then  -- on rising_edge == Clk'event and Clk
-				Q <= Q +1  ;
-			 end if;
-		 end if;	
-	end process pcompteur;
-
+ 
+    pCompt_1 : process(clk, arst_n)
+    begin 
+         if arst_n = '0' then 
+            Q1 <= (others => '0');
+         elsif rising_edge(clk) then
+            if SRst = '1' then
+                Q1 <= (others => '0');
+            elsif en = '1' then  -- on rising_edge == Clk'event and Clk
+                Q1 <= Q1 + 1;
+            end if;
+         end if;
+         
+    end process pCompt_1;
+ 
+    q<=Q1;  
+ 
 end architecture rtl;
